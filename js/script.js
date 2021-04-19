9531cf87bfc2ad7d4098b80f641b1302c9cdb4b8
NoeFabris <noe@elysresidence.com>
2021-04-19 18:45:56 +0200

    Fine

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ var subBtn = document.getElementById('button')
 
 subBtn.addEventListener('click', function(){
     var hambName = document.getElementById('name')
-    // if (hanbName === 'ciao') {
-    //     prompt('ciao')
-    // }else {
-    //     alert(df)
-    // }
-
+    console.log(hambName.value)
+    
+    var couponUser = document.getElementById('coupon')
+    console.log(couponUser.value)
+    
     var listaIngredienti = document.querySelectorAll(".ingredient [type = 'checkbox']")
     console.log(listaIngredienti)
+
     var somma = 50
     
     for (let i = 0; i < listaIngredienti.length; i++) {
@@ -19,6 +19,31 @@ subBtn.addEventListener('click', function(){
             somma += parseInt(listaIngredienti[i].value)
             console.log(prezzoAgg)
         }
+    }
+
+    if (!hambName.value) {
+        alert('Manca il nome dell burger')
+    }
+
+    switch (couponUser.value) {
+        case 'Sconto50':
+            somma -= ((somma * 50) / 100)
+            break;
+    
+        case 'Sconto25':
+            somma -= ((somma * 25) / 100)
+            break;
+        
+        case 'Sconto10':
+            somma -= ((somma * 10) / 100)
+            break;
+
+        case 'Sconto5':
+            somma -= ((somma * 5) / 100)
+            break;
+            
+        default:
+            break;
     }
     document.getElementById('price').innerHTML = somma
 })
